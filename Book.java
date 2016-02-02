d83846b885825a9987aec93dd8b537358be37b7f
Gloria <ngang@bxscience.edu>
2016-02-02 09:33:45 -0500

    Created file.
    Added fields and constructor.

---
+++ Book.java
@@ -0,0 +1,13 @@
+
+public class Book{
+  private String title, author, status;
+  private double borrowDate;
+  private boolean isAvailable;
+  
+  //Constructor.
+  public Book(String t, String a, String s){
+    title = t;
+    author = a;
+    status = s;
+  }
+}
