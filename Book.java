4015ae42fbca6cec3e7f838380457d071c7dfd74
Gloria <ngang@bxscience.edu>
2016-02-03 17:37:43 -0500

    Update Book.java
    Added methods to mark availability of a book.

--- Book.java
+++ Book.java
@@ -1,13 +1,29 @@
+/* Author: Gloria Ngan 
+ * 
+ * This is a class for book objects, which have unique
+ * attributes like titles, authors, physical states,
+ * and availibility.
+ */
 
 public class Book{
-  private String title, author, status;
-  private double borrowDate;
-  private boolean isAvailable;
+  private String title, author, status;     //For title, author, and the physical condition of the book.
+  private double borrowDate;                //Double for the date the book was checked out.
+  private boolean isAvailable = true;       //Boolean for whether the book is borrowed or not.
   
   //Constructor.
   public Book(String t, String a, String s){
     title = t;
     author = a;
     status = s;
+  }
+  
+  //Modifier method to mark book as unavailible.
+  public void makeBorrowed(){
+    isAvailable = false;
+  }
+  
+  //Modifier method to mark a book as availible.
+  public void makeReturned(){
+    isAvailable = true;
   }
 }
