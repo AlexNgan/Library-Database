ca80d2a1f6ff55a1e959d4d1ce357c4bed28dfed
Gloria <ngang@bxscience.edu>
2016-02-03 22:03:59 -0500

    Create StudentBorrower.java

---
+++ StudentBorrower.java
@@ -0,0 +1,13 @@
+/* Author: Gloria Ngan
+ * 
+ * This class regulates students borrowing books. Restrictions
+ * include a two book limit and a 
+ */ 
+
+public class StudentBorrower extends Borrower{
+  //Fields for each borrower's info.
+  private String OSIS;
+  private String name;
+  private int grade;
+  private String offCLass;
+}
