b0c1d92336cf57281d7d7c9412a6f7bf763ac24d
crystal <leecr@bxscience.edu>
2016-02-09 01:23:10 -0500

    Update Book.java

--- Book.java
+++ Book.java
@@ -9,12 +9,14 @@ public class Book{
   private String title, author, status;     //For title, author, and the physical condition of the book.
   private double borrowDate;                //Double for the date the book was checked out.
   private boolean isAvailable = true;       //Boolean for whether the book is borrowed or not.
+  private int isbn;                         //???? Not sure if she wanted us to include this?????
   
   //Constructor.
-  public Book(String t, String a, String s){
-    title = t;
-    author = a;
-    status = s;
+  public Book(String t, String a, String s, int isbn){
+    this.title = t;
+    this.author = a;
+    this.status = s;
+    this.isbn = isbn;
   }
   
   //Modifier method to mark book as unavailible.
@@ -25,5 +27,10 @@ public class Book{
   //Modifier method to mark a book as availible.
   public void makeReturned(){
     isAvailable = true;
+  }
+  
+  @Override
+  public String toString(){
+    return "\nTitle: " + title + "\nAuthor: " + author + "\nISBN: " + isbn + "\nStatus: " + status;
   }
 }
