3c6a8d21e6d03c4d1cef4ad901b5a5d4cc7b3206
crystal <leecr@bxscience.edu>
2016-02-10 15:11:25 -0500

    Update LibraryTester.java

--- LibraryTester.java
+++ LibraryTester.java
@@ -1,11 +1,5 @@
 import java.util.Scanner;
 
-/* Author: Gloria Ngan (all right but what about me, Crystal Lee)
- * 
- * This is the main class for the library database. Requires
- * Book, Borrower, Library, StudentBorrower, and TeacherBorrower.
- */ 
-
 public class LibraryTester{
   
   static String fileName = null;
