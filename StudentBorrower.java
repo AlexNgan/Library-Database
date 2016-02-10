c4b0016028776b3a203ecc2d025d773373cd022e
crystal <leecr@bxscience.edu>
2016-02-10 15:11:34 -0500

    Update StudentBorrower.java

--- StudentBorrower.java
+++ StudentBorrower.java
@@ -1,10 +1,3 @@
-/* Author: Gloria Ngan
- * 
- * This class regulates students borrowing books. Restrictions
- * include a two book limit and a two week limit on borrowed
- * material.
- */ 
-
 public class StudentBorrower extends Borrower{
   //Fields for each borrower's info.
   private String OSIS;
