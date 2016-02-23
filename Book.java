bdf45c0bcdff2583f75478ed4e76ffd5023eeb62
Gloria Ngan <ngang@bxscience.edu>
2016-02-23 00:08:47 -0500

    Update Book.java
    Added fields.

--- Book.java
+++ Book.java
@@ -6,14 +6,16 @@
  */
 
 public class Book{
-  private String title, author, status;     //For title, author, and the physical condition of the book.
-  private double borrowDate;                //Double for the date the book was checked out.
-  private boolean isAvailable = true;       //Boolean for whether the book is borrowed or not.
+  private String title, author, status, ISBN, genre;     //For title, author, and the physical condition of the book.
+  private double borrowDate;                      //Double for the date the book was checked out.
+  private boolean isAvailable = true;             //Boolean for whether the book is borrowed or not.
   
-  //Constructor.
-  public Book(String t, String a, String s){
+  //Constructor. Params for ISBN, title, author, genre, status.
+  public Book(String i, String t, String a, String g, String s){
+    ISBN = i;
     title = t;
     author = a;
+    genre = g;
     status = s;
   }
   
