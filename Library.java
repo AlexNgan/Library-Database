187f037661d2f2b5ade781e7d6a440ce9d7331cb
Gloria Ngan <ngang@bxscience.edu>
2016-03-05 16:32:39 -0500

    Update Library.java

--- Library.java
+++ Library.java
@@ -12,11 +12,14 @@ import java.util.ArrayList;
  */
 
 public class Library{
-
-  private ArrayList<Book> database = new ArrayList<Book>();     //Holds all books.
+  
+  private ArrayList<Book> database = new ArrayList<Book>();                           //Holds all books.
+  private ArrayList<TeacherBorrower> teachers = new ArrayList<TeacherBorrower>();     //Holds all teachers.
+  private ArrayList<StudentBorrower> students = new ArrayList<StudentBorrower>();     //Holds all students.
+  
   
   //Method to import books from a .txt file.
-  public void importBook(String fileName){
+  public void importBooks(String fileName){
     String file = fileName;     // The file to open.
     String line;
     
@@ -30,10 +33,67 @@ public class Library{
       //Saves each line as a book object.
       while((line = bufferedReader.readLine()) != null) {
         String[] str = line.split(", ");     // Takes a single line and saves info as an array
-                                             // with the format: [ISBN, title, author, etc.]
+        // with the format: [ISBN, title, author, etc.]
         
         Book book = new Book(str[0], str[1], str[2], str[3], str[4]);    //Creates new book with elements in array.
         database.add(book);                                              //Adds new book to library.
+      }   
+      
+      bufferedReader.close();   //Close reader.      
+    }
+    catch(FileNotFoundException ex) {
+      System.out.println("Unable to open file '" + file + "'");                
+    }
+    catch(IOException ex) {
+      System.out.println("Error reading file '" + file + "'");                  
+    }    
+  }
+  
+  public void importTeachers(String fileName){
+    String file = fileName;     // The file to open.
+    String line;
+    
+    try {
+      // FileReader reads text files in the default encoding.
+      FileReader fileReader = new FileReader(file);
+      
+      //Wrap FileReader in BufferedReader.
+      BufferedReader bufferedReader = new BufferedReader(fileReader);
+      
+      //Saves each line as a book object.
+      while((line = bufferedReader.readLine()) != null) {
+        String[] str = line.split(", ");     
+        TeacherBorrower teacher = new TeacherBorrower(str[1], str[0]); 
+        teachers.add(teacher);
+      }   
+      
+      bufferedReader.close();   //Close reader.      
+    }
+    catch(FileNotFoundException ex) {
+      System.out.println("Unable to open file '" + file + "'");                
+    }
+    catch(IOException ex) {
+      System.out.println("Error reading file '" + file + "'");                  
+    }    
+  }
+  
+  public void importStudents(String fileName){
+    String file = fileName;     // The file to open.
+    String line;
+    
+    try {
+      // FileReader reads text files in the default encoding.
+      FileReader fileReader = new FileReader(file);
+      
+      //Wrap FileReader in BufferedReader.
+      BufferedReader bufferedReader = new BufferedReader(fileReader);
+      
+      //Saves each line as a book object.
+      while((line = bufferedReader.readLine()) != null) {
+        String[] str = line.split(", ");     
+        
+        StudentBorrower student = new StudentBorrower(str[0], str[1], str[2], str[3]); 
+        students.add(student);                                           
       }   
       
       bufferedReader.close();   //Close reader.      
@@ -53,13 +113,27 @@ public class Library{
     }
   }
   
+  //Prints out the contents of the Library.
+  public void displayTeachers(){
+    for(int i = 0; i < teachers.size(); i++){
+      System.out.println(teachers.get(i));
+    }
+  }
+  
+  //Prints out the contents of the Library.
+  public void displayStudents(){
+    for(int i = 0; i < students.size(); i++){
+      System.out.println(students.get(i));
+    }
+  }
+  
   //Modifier method to add a books to database.
-  public void addNewBook(Book book){
+  public void addBook(Book book){
     database.add(book);
   }
   
   //Modifier method to remove books from database.
-  public void removeOldBook(Book book){
+  public void removeBook(Book book){
     database.remove(book);
   }
   
